48c95ea7a1b7f4d934b344f42f29dc88fc2c9b23
unknown <huangzhifu@JINKE.WORK>
2021-07-03 11:34:52 +0800

    修改

--- cmd/main.go
+++ cmd/main.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	c := cron.New()
-	c.AddFunc("0 */1 * * * ?",remindexpireaccount.Remindaccount)
+	//c.AddFunc("0 */1 * * * ?",remindexpireaccount.Remindaccount)
 	//每天10点执行
-	//c.AddFunc("0 0 10 * * ?",remindexpireaccount.Remindaccount)
+	c.AddFunc("0 0 10 * * ?",remindexpireaccount.Remindaccount)
 
 	go c.Start()
 	select {
