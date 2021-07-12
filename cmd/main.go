da261f00da8cf342bfd2d7117917d559ec71217c
huangzhif <394320736@qq.com>
2021-07-12 11:40:03 +0800

    修改cron调用频率

--- cmd/main.go
+++ cmd/main.go
@@ -9,14 +9,14 @@ import (
 
 func main() {
 	c := cron.New()
-     //Minutes, Hours, Day of month, Month, Day of week
+     //Seconds,Minutes, Hours, Day of month, Month, Day of week
 	//c.AddFunc("0 */1 * * * ?",remindexpireaccount.Remindaccount)
 	//每天10点执行:账号即将过期提醒
 	c.AddFunc("0 0 10 * * *",remindexpireaccount.Remindaccount)
 	//每天9点执行：新增删除账号信息
 	c.AddFunc("0 0 9 * * *",remindadddelaccount.Adddelaccount)
 	//每天23:55：禁用到期账户
-	c.AddFunc("55 23 * * *",disableaccount.Disableaccount)
+	c.AddFunc("0 55 23 * * *",disableaccount.Disableaccount)
 
 	go c.Start()
 	select {
